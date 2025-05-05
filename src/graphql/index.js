982764b73c50471d9aff80e5b8fb1ee27aaf3530
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2025-05-05 17:10:08 -0500

    Separate typing

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,21 +1,28 @@
-const { ApolloServer } = require('apollo-server-express');
-const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
-
-const typeDefs = `
-  type Query {
-    hello: String
-  }
-`;
+const { ApolloServer } = require("apollo-server-express");
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
+const { loadFiles } = require("@graphql-tools/load-files");
 
 const resolvers = {
   Query: {
-    hello: () => 'Hola, mundo',
+    hello: () => "Hola, mundo",
+    getProduct: () => {
+      return {
+        id: 1,
+        name: "Product 1",
+        price: 10,
+        description: "Description of product 1",
+        image: "https://example.com/image1.jpg",
+        createdAt: new Date(),
+      };
+    },
   },
 };
 
 const startServer = async (app) => {
   const server = new ApolloServer({
-    typeDefs,
+    typeDefs: await loadFiles("./src/**/*.graphql"),
     resolvers,
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
   });
